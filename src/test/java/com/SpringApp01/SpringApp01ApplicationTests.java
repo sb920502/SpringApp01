df7285b764df33d2c65cf46d1dc20959adcd3d72
sb920502 <bhowmik920502@gmail.com>
2021-12-26 12:26:19 +0600

    first commit

---
+++ src/test/java/com/SpringApp01/SpringApp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringApp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
